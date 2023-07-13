55ad906b14f18a5c48b3e00b3eccce19356a3894
Inganathi <inganathi2001@gmail.com>
2023-07-13 15:24:29 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+*/
+int main(void)
+{
+	wire(2,
+	"and that piece of that art is useful\" -Dora Korpar, 2015-1019\n",59);
+	return (1);
+}	
